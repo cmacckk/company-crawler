dcc61945605d1db11eb63fccdb2137c367804c5a
cmacckk <emailforgty@163.com>
2024-03-11 15:05:56 +0800

    修改天眼查读取信息
    修改天眼查读取信息

--- tianyancha.py
+++ tianyancha.py
@@ -7,17 +7,35 @@
 from tianyancha import crawler
 from util import log
 import urllib3
+import logging
+import pandas as pd
 urllib3.disable_warnings()
 
 
 log.set_file("./logs/tianyancha.log")
 
+# excel列名
+COLUMNS = ['公司名称', '统一社会信用代码', '公司地址']
+
+# excel表名
+OUTPUT_XLSX = './天眼查导出表.xlsx'
+
+# 工作表名
+SHEET_NAME = '天眼查'
+
 
 if __name__ == '__main__':
-    keys = ['谷歌']
-    crawler.load_keys(keys)
-    crawler.start()
-
+    result = []
+    with open('./keys.txt', 'r', encoding='utf-8') as f:
+        keys = f.read().strip().split('\n')
+        for key in keys:
+            companies = crawler.start(key)
+            for company in companies:
+                result.append([company.name, company.credit_code, company.company_address])
+    df = pd.DataFrame(result, columns = COLUMNS)
+    # 导出为Excel时不带有索引
+    df.to_excel(OUTPUT_XLSX, SHEET_NAME, index=False)
+    logging.info(f"excel文件已导出到[ {OUTPUT_XLSX} ], 工作表名为 [ {SHEET_NAME} ]")
 
 
 
--- tianyancha/crawler.py
+++ tianyancha/crawler.py
@@ -9,26 +9,20 @@ from tianyancha.client import TycClient
 from db.mysql_connector import *
 
 
-def start():
+def start(key):
     """ 入口函数 """
     def __printall(items):
         for elem in items:
             logging.info(elem.__str__())
 
-    keys = globals().get('keywords', [])
-    for key in keys:
-        logging.info('正在采集[%s]...' % key)
-        companies = TycClient().search(key).companies
-        # 写入db
-        # insert_company(companies)
-        __printall(companies)
-    logging.info("completed")
+    logging.info(f'正在采集[ {key} ]...')
+    companies = TycClient().search(key).companies
+    # 写入db
+    # insert_company(companies)
+    # __printall(companies)
+    logging.info(f"采集[ {key} ] 完成")
+    return companies
 
 
-def load_keys(keys: list):
-    globals().setdefault('keywords', keys)
-
-
-
-
-
+# def load_keys(keys: list):
+#     globals().setdefault('keywords', keys)
